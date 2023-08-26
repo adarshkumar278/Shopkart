a42e97b0fbcc94b784a01f79ea4cdd05c2634176
Adarsh Sharma <sharmaadarsh278@gmail.com>
2023-08-26 09:33:34 +0530

    initial folder setup commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './components/layout/header'
+import Home from './pages/home'
+
+function App() {
+
+  return (
+    <>
+     <Header />
+     <Home/>
+     <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Banner from '../components/pages/home/banner'
+import ProductList from '../components/pages/home/product-list'
+
+const Home = () => {
+  return (
+    <>
+    <Banner />
+    <ProductList />
+    </>
+  )
+}
+
+export default Home
