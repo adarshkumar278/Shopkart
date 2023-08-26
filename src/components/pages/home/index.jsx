0d919a1fe2d17545813bb6a392de63aca7343333
Adarsh Sharma <sharmaadarsh278@gmail.com>
2023-08-26 09:43:52 +0530

    folder naming conventions corrected

---
+++ src/components/pages/home/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Banner from './banner'
+import ProductList from './product-list'
+
+const Index = () => {
+  return (
+    <>
+    <Banner />
+    <ProductList />
+    </>
+  )
+}
+
+export default Index
---
+++ src/pages/home.jsx
@@ -0,0 +1,10 @@
+import React from 'react' 
+import Index from '../components/pages/home'
+
+const Home = () => {
+  return (
+    <Index />
+  )
+}
+
+export default Home
